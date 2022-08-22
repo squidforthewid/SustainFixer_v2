9e2c1c0fb03af5e7bad2536506fb6900216c3d12
805cameron <805cameron@gmail.com>
2022-08-21 21:28:50 -0700

    Cleanup pt 2

--- SustainFixer_v2/Program.cs
+++ SustainFixer_v2/Program.cs
@@ -12,13 +12,6 @@ namespace SustainFixer
     {
         static void Main(string[] args)
         {
-#if DEBUG
-            args = new string[]
-            {
-                "C:\\Users\\805ca\\OneDrive\\Documents\\GitHub\\SustainFixer_v2\\3 Doors Down - Kryptonite\\notes - Copy.chart"
-            };
-#endif
-
             //Key: file extension to search for
             //Value: method to be called when that extension is found
             FileProcessor.fileTypeMap = new Dictionary<string, Action<string>>()
